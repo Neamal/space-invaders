9b58c2d2425d911091d4e15e0894d75f604a8c4b
Sanni5914 <86491034+Sanni5914@users.noreply.github.com>
2022-05-26 12:37:03 -0700

    Add files via upload

--- Breakout.java
+++ Breakout.java
@@ -1,219 +1,169 @@
-package breakout;
+package pong;
 
 import java.awt.Color;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.event.KeyEvent;
-import java.util.Set;
-
-import javax.swing.plaf.SliderUI;
-
 import java.awt.Font;
+import java.awt.Graphics2D;
+import java.awt.event.KeyEvent;
 
 import utilities.GDV5;
-import utilities.SoundDriverHo;
 
 public class Breakout extends GDV5{
 	
-	boolean start = false;
-	boolean	freeze = true;
-	int gamemode = 0;
-	Ball b1 = new Ball();
-	Paddle p1 = new Paddle(Breakout.getMaxWindowX()/2 - 60, Breakout.getMaxWindowY() - 60, 140, 20);
-	Scoreboard s = new Scoreboard();
-	static int P1Score = 0;
-	private int lives = 9;
-	static int count = 0;
-	private int level = 1;
-	static Brick[] bricks;
-	private SoundDriverHo s1;
-	private SoundDriverHo s2;
-	private String[] filenames = new String[1];
-	Images image = new Images();
-	
-	public Breakout() {
-		filenames[0] = "crunch.wav";
-		s1 = new SoundDriverHo(filenames, this);
-		filenames[0] = "lofiintro.wav";
-		s2 = new SoundDriverHo(filenames, this);
-	}
+	BreakoutBall one = new BreakoutBall(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY()/2, 20, 20);
+	BreakoutBall two = new BreakoutBall(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY()/2, 40, 40);
+	BreakoutPaddle paddle1 = new BreakoutPaddle(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY()-30, 90, 20);
+	BreakoutScoreboard s = new BreakoutScoreboard();
+	static int gameState = 0;
+	static int level = 4;
+	static int lives = 5;
+	private boolean check = true;
 	
 	public static void main(String[] args) {
-
-		Breakout b = new Breakout();
-		b.start();
-		Color[] colors = {Color.green, Color.cyan, Color.blue, Color.white, Color.magenta, Color.orange, Color.yellow, Color.pink, Color.red};
-		bricks = new Brick[8];
-		int x = 2, y = 2;
-		int bricksPerRow = 8;
-		int count = 1, colorIndex = 0;
-		for (int i = 0; i < bricks.length; i++) {
-			bricks[i] = new Brick(x, y, colors[colorIndex % colors.length]);
-			x += bricks[i].getW();
-			if (count % bricksPerRow == 0) {
-				x = 2;
-				y += bricks[i].getHeight() + 2;
-				colorIndex++;
-			}
-			count++;
-			System.out.println(i + " " + bricks[i].getX() + " " + bricks[i].getY());
-		}
-
+		BreakoutBrick.makeBricks(104);
+		Breakout b1 = new Breakout();
+		b1.start();
 	}
 	
- 	public void levelChange() {
-		if (P1Score == 8 && b1.intersects(p1)) {
-			Color[] colors = {Color.green, Color.cyan, Color.blue, Color.white, Color.magenta, Color.orange, Color.yellow, Color.pink, Color.red};
-			bricks = new Brick[16];
-			int x = 2, y = 2;
-			int bricksPerRow = 8;
-			int count = 1, colorIndex = 0;
-			for (int i = 0; i < bricks.length; i++) {
-				bricks[i] = new Brick(x, y, colors[colorIndex % colors.length]);
-				x += bricks[i].getW();
-				if (count % bricksPerRow == 0) {
-					x = 2;
-					y += bricks[i].getHeight() + 2;
-					colorIndex++;
-				}
-				count++;
-				System.out.println(i + " " + bricks[i].getX() + " " + bricks[i].getY());
-			}
-			b1.setLocation(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY() - 100);
+	
+	public void powerUp() {
+		if (level == 2) {
+			one.height = 40;
+			one.width = 40;
+		}
+		if (level == 3) {
+			paddle1.width = 140;
 		}
 		
-		if (P1Score == 24 && b1.intersects(p1)) {
-			Color[] colors = {Color.green, Color.cyan, Color.blue, Color.white, Color.magenta, Color.orange, Color.yellow, Color.pink, Color.red};
-			bricks = new Brick[32];
-			int x = 2, y = 2;
-			int bricksPerRow = 8;
-			int count = 1, colorIndex = 0;
-			for (int i = 0; i < bricks.length; i++) {
-				bricks[i] = new Brick(x, y, colors[colorIndex % colors.length]);
-				x += bricks[i].getW();
-				if (count % bricksPerRow == 0) {
-					x = 2;
-					y += bricks[i].getHeight() + 2;
-					colorIndex++;
-				}
-				count++;
-				System.out.println(i + " " + bricks[i].getX() + " " + bricks[i].getY());
-			}
-			b1.setLocation(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY() - 100);
-		}
 	}
 	
-	public void update() {
-		p1.controlPaddle();
-		b1.intersectBrick(bricks);
-		if (!freeze) b1.move(b1, p1);
-		s.P1Score = P1Score;
-		for (int i = 0; i < bricks.length; i++) {
-			if (bricks[i].getVis() == true && b1.intersects(bricks[i])) {
-				P1Score ++;
-				s1.play(0);
-			}
-		}
-		if (!start) s2.play(0);
-		if (s2.isPlaying(0) == false) s2.play(0);
-		if (b1.y >= Breakout.getMaxWindowY()) lives--;
-		if (P1Score == 8 && b1.intersects(p1)) level++;
-		if (P1Score == 24 && b1.intersects(p1)) level++;
-	}
-
-	public void draw(Graphics2D win) {
-				
-		if (!start) {
-			win.drawImage(image.gif, Breakout.getMaxWindowX()/2 - 120, Breakout.getMaxWindowY() - 350, 240, 300, null);
-			win.setColor(Color.PINK);
-			win.setFont(new Font("Arial", Font.BOLD, 50));
-			win.drawString("Breakout", Breakout.getMaxWindowX()/2 - 120, 100);
-			win.setFont(new Font("Arial", Font.BOLD, 20));
-			win.drawString("By: Junhyung Yoon", Breakout.getMaxWindowX()/2 - 110, 150);
-			win.setFont(new Font("Arial", Font.BOLD, 30));
-			win.drawString("Press Enter to Play", 160, 200);
-			win.setFont(new Font("Times New Roman", Font.ITALIC, 20));
-			win.drawString("Press 'a' to move your paddle left and 'd' to move it to the right", 50, 280);
-			win.drawString("Try to pass the third level without expending your 5 lives!", 60, 330);
-			win.drawString("Each time you can't return the ball, you lose a life", 100, 380);
-			win.drawString("Press 'f' to make the ball start moving", 140, 420);
-			lives = 5;
-			P1Score = 0;
-			b1.setLocation(GDV5.getMaxWindowX()/2, GDV5.getMaxWindowY() - 100);
+	public void runGame1() {
+		
+	
+		one.move(one, paddle1);
+		paddle1.controlPaddle();
+		one.brickHit();
+		powerUp();
+		if (one.Score1 == 2080 && check) {
+			level++;
+			BreakoutBrick.makeBricks(128);
+			check = false;
+			one.setLocation(Breakout.getMaxWindowX()/2, Breakout.getMaxWindowY()/2);
 			
 		}
-		
-		if (GDV5.KeysTyped[KeyEvent.VK_ENTER]) {
-			win.drawImage(image.background, 0, 0, Breakout.getMaxWindowX(), Breakout.getMaxWindowY(), null);
-			start = true;
-			win.setColor(Color.WHITE);
-			win.draw(b1);
-			win.fill(b1);
-			win.setColor(Color.RED);
-			win.draw(p1);
-			win.fill(p1);
-			Brick.drawBricks(win, bricks);
-			s.Scoreboard(win);
-			win.setColor(Color.WHITE);
-			win.setFont(new Font("Arial", Font.BOLD, 20));
-			win.drawString("Lives: " + lives, Breakout.getMaxWindowX()/2 - 240, Breakout.getMaxWindowY() - 80);
-			win.drawString("Level: " + level, Breakout.getMaxWindowX() - 140, Breakout.getMaxWindowY() - 80);
-			if (P1Score >= 8) p1.setSize(200,10); 
-			if (P1Score >= 24) b1.setSize(40,40);
-			levelChange();
+		if (one.Score1 == 2100) {
+			check = true;
+		}
+		if (one.Score1 == 4640 && check) {
+			level++;
+			BreakoutBrick.makeBricks(152);
+			one.setLocation(Breakout.getMaxWindowX()/2, Breakout.getMaxWindowY()/2);
+			check = false;
+		}
+		if (one.Score1 == 4660) {
+			check = true;
+		}
+		if (one.Score1 == 7680 && check) {
+			level++;
+			BreakoutBrick.makeBricks(168);
+			one.setLocation(Breakout.getMaxWindowX()/2, Breakout.getMaxWindowY()/2);
+			check = false;
+		}
+		if (one.Score1 == 9160) {
+			gameState = 4;
+		}
+		if(one.dY == GDV5.getMaxWindowY()) {
+			lives-=1;
+		}
+		if(level == 4) {
+			two.move(two, paddle1);
+			two.brickHit();
 		}
 
-		if (GDV5.KeysTyped[KeyEvent.VK_F]) {
-			freeze = false;
 		}
 		
-		if (lives <= 0) {
-			GDV5.KeysTyped[KeyEvent.VK_ENTER] = false;
-			win.setColor(Color.RED);
-			win.setFont(new Font("Arial", Font.BOLD, 50));
-			win.drawString("Gameover", Breakout.getMaxWindowX()/2 - 120, 100);
-			win.setFont(new Font("Arial", Font.ITALIC, 50));
-			win.drawString("Press ESCAPE to go back to menu", Breakout.getMaxWindowX()/2 - 300, 200);
-			p1.setSize(140,20);
-			b1.setSize(20,20);
+	public int level(double bx) {
+		if(one.y == Breakout.getMaxWindowY()) {
+			lives -= 1;
+		}
+		return lives;
+	}
+	
+	public void setGameState() {
+		if (GDV5.KeysPressed[KeyEvent.VK_1]) gameState = 1;
+		if (GDV5.KeysPressed[KeyEvent.VK_2]) gameState = 2;
+		if (GDV5.KeysPressed[KeyEvent.VK_3]) gameState = 3;
+		if (GDV5.KeysPressed[KeyEvent.VK_4]) gameState = 4;
+		if (GDV5.KeysPressed[KeyEvent.VK_ESCAPE]) gameState = 0;
+		if (GDV5.KeysPressed[KeyEvent.VK_R]) {
+			BreakoutBrick.makeBricks(104);
+			one.setLocation(Breakout.getMaxWindowX()/2, Breakout.getMaxWindowY()/2);
+			one.width = 20;
+			one.height = 20;
+			gameState = 0;
+			one.Score1 = 0;
 			level = 1;
-		}
-		
-		if (GDV5.KeysPressed[KeyEvent.VK_ESCAPE]) {
-			start = false;
-			freeze = true;
-			GDV5.KeysTyped[KeyEvent.VK_ENTER] = false;
-			GDV5.KeysTyped[KeyEvent.VK_F] = false;
-			p1.setLocation(Breakout.getMaxWindowX()/2 - 60, Breakout.getMaxWindowY() - 60);
-			Color[] colors = {Color.green, Color.cyan, Color.blue, Color.white, Color.magenta, Color.orange, Color.yellow, Color.pink, Color.red};
-			bricks = new Brick[8];
-			int x = 2, y = 2;
-			int bricksPerRow = 8;
-			int count = 1, colorIndex = 0;
-			for (int i = 0; i < bricks.length; i++) {
-				bricks[i] = new Brick(x, y, colors[colorIndex % colors.length]);
-				x += bricks[i].getW();
-				if (count % bricksPerRow == 0) {
-					x = 2;
-					y += bricks[i].getHeight() + 2;
-					colorIndex++;
-				}
-				count++;
-				System.out.println(i + " " + bricks[i].getX() + " " + bricks[i].getY());
-			}
-		}
-		
-		if (P1Score >= 56) {
-			GDV5.KeysTyped[KeyEvent.VK_ENTER] = false;
-			win.setColor(Color.GREEN);
-			win.setFont(new Font("Arial", Font.BOLD, 50));
-			win.drawString("You Won!", Breakout.getMaxWindowX()/2 - 120, 100);
-			win.setFont(new Font("Arial", Font.ITALIC, 50));
-			win.drawString("Press ESCAPE to go back to menu", Breakout.getMaxWindowX()/2 - 300, 200);
-			p1.setSize(140,20);
-			b1.setSize(20,20);
-			level = 1;
+			lives = 5;
 		}
 	}
 	
+	
+	
+	public void update() {
+		setGameState();
+		if (gameState == 1) {
+			runGame1();
+			level(one.dY);
+		}
+		if(lives == 0) {
+			gameState = 3;
+		}
+		//BreakoutBrick.bricks[index].setIsVis();
+		//if (drawPart == 0) {
+			//particle.makeParticles(BreakoutBrick.bricks[index]);
+		//}
+		//drawPart = 1;
+		//particle.moveParticles();
+		//count++;
+		//if (count % 60 == 0) {
+			//index += 1;
+			//drawPart = 0;
+		//}	
+	}	
+	public void draw(Graphics2D win) {
+		if(gameState == 1) {	
+		for (BreakoutBrick b: BreakoutBrick.getBricks()) {
+		b.draw(win);
+		win.setColor(Color.MAGENTA);
+		win.draw(one);
+		win.fill(one);
+		win.setColor(Color.RED);
+		win.draw(paddle1);
+		win.fill(paddle1);
+		one.Score(win);
+		Font f1 = new Font("TimesNewRoman", Font.BOLD, 15);
+		win.setFont(f1);
+		win.drawString("Level " + level, 20, Breakout.getMaxWindowY()-20);
+		win.drawString("Lives: " + lives, Breakout.getMaxWindowX()-75, Breakout.getMaxWindowY()-20);
+		}
+		if (level == 4) {
+			win.setColor(Color.orange);
+			win.draw(two);
+			win.fill(two);
+		}
+		}
+		else if (gameState == 0) {
+		s.splash(win);
+	}
+		else if (gameState == 2) {
+		s.howToPlay(win);
+	}
+		else if(gameState == 3) {
+		s.lose(win);	
+		}
+		else if(gameState == 4) {
+		s.winner(win);
+		}
 }
+}
+	
+
